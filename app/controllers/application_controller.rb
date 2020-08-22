4da0ed70269dc52e335ecace1939183030134b03
AnaTeresaDona <anatdona@gmail.com>
2020-08-21 20:15:27 -0400

    Project Tallarín Forth commit, making it pretty and using pagination with kaminari

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
     protected
     def configure_permited_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :photo_url])
+        devise_parameter_sanitizer.permit(:sign_up, keys:[:username, :photo_url])
     end
 
 end
