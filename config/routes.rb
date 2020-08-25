b586d3f4e58b6c0c16c8c5615f028438b3a7859e
AnaTeresaDona <anatdona@gmail.com>
2020-08-25 17:05:27 -0400

    Project Tallarín grey and wellow beautification

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'home/index'
   
   devise_for :users
-  devise_for :views
+
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
